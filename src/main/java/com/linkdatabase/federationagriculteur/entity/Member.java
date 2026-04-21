473d7cd0d0571728b849d52ba5fa67f62e4fda5a
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 22:10:32 +0300

    suppr the true of equals and hascode

--- src/main/java/com/linkdatabase/federationagriculteur/entity/Member.java
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/Member.java
@@ -5,7 +5,7 @@ import lombok.EqualsAndHashCode;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode
 public class Member extends MemberInformation {
     private String id;
     private List<Member> referees;
