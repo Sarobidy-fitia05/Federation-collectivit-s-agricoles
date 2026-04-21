55a9112c38b3bfa443385dcfc2203b3864367aab
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 20:38:38 +0300

    add CreateMemberRequest content

--- src/main/java/com/linkdatabase/federationagriculteur/entity/CreateMemberRequest.java
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/CreateMemberRequest.java
@@ -1,22 +1,13 @@
 package com.linkdatabase.federationagriculteur.entity;
 
 import lombok.Data;
-import java.time.LocalDate;
+import lombok.EqualsAndHashCode;
 import java.util.List;
 
 @Data
-public class CreateMemberRequest {
-     private String firstName;
-    private String lastName;
-    private LocalDate birthDate;
-    private Gender gender;
-    private String address;
-    private String profession;
-    private Integer phoneNumber;
-    private String email;
-    private MemberOccupation occupation;
-
-     private String collectivityIdentifier;
+@EqualsAndHashCode
+public class CreateMemberRequest extends MemberInformation {
+    private String collectivityIdentifier;
     private List<String> referees;
     private Boolean registrationFeePaid;
     private Boolean membershipDuesPaid;
