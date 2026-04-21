3dc6c702d138a87de977a393995ae6968d860e2f
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 15:53:14 +0300

    add class Role

---
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/Role.java
@@ -0,0 +1,10 @@
+package com.linkdatabase.federationagriculteur.entity;
+
+public enum Role {
+    PRESIDENT,
+    VICE_PRESIDENT,
+    TREASURER,
+    SECRETARY,
+    CONFIRMED_MEMBER,
+    JUNIOR_MEMBER
+}
