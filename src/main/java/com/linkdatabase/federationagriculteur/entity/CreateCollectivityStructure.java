4a1ce67e604ae82609cd953f298b6388b8719a73
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 22:39:43 +0300

    add CollectityStructure

---
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/CreateCollectivityStructure.java
@@ -0,0 +1,11 @@
+package com.linkdatabase.federationagriculteur.entity;
+
+import lombok.Data;
+
+@Data
+public class CreateCollectivityStructure {
+    private String president;
+    private String vicePresident;
+    private String treasurer;
+    private String secretary;
+}
