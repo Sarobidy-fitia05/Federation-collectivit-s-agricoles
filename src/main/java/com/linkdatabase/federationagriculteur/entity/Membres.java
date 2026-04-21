59b62a92f2ea9be5ed1f3ba611a69b4d95924a6f
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 15:41:26 +0300

    add class Gender

--- src/main/java/com/linkdatabase/federationagriculteur/entity/Membres.java
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/Membres.java
@@ -1,8 +1,17 @@
 package com.linkdatabase.federationagriculteur.entity;
 
+import java.time.LocalDate;
+
 public class Membres {
     private int id;
     private String firstName;
     private String lastName;
-    private Enum
+    private LocalDate birthDate;
+    private Gender gender;
+    private String address;
+    private String job;
+    private String phone;
+    private String email;
+    private LocalDate membershipDate;
+
 }
