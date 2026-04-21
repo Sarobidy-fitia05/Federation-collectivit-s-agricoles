17d74a9655f1e7acff1c4834c2c1949ffc8733bd
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 15:36:22 +0300

    add some classes in memmbers

--- src/main/java/com/linkdatabase/federationagriculteur/entity/Membres.java
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/Membres.java
@@ -1,5 +1,8 @@
 package com.linkdatabase.federationagriculteur.entity;
 
 public class Membres {
-
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Enum
 }
