9b57b214499bc2bdce4e7be2fefe7d066068c6d0
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-21 12:08:37 +0300

    first commit

---
+++ src/main/java/com/linkdatabase/federationagriculteur/FederationAgriculteurApplication.java
@@ -0,0 +1,13 @@
+package com.linkdatabase.federationagriculteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationAgriculteurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FederationAgriculteurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkdatabase/federationagriculteur/FederationAgriculteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkdatabase.federationagriculteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationAgriculteurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
