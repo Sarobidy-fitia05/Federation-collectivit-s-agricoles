a3ae87442e6ad10da1155b818a3d072c072e63ac
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-22 15:45:36 +0300

    add the method in service List<MembershipFee> getMembershipFees(String collectivityId) which will find the existence of the collectivity and claim the fee

--- src/main/java/com/linkdatabase/federationagriculteur/service/MembershipFeeService.java
+++ src/main/java/com/linkdatabase/federationagriculteur/service/MembershipFeeService.java
@@ -1,7 +1,10 @@
 package com.linkdatabase.federationagriculteur.service;
 
+import com.linkdatabase.federationagriculteur.entity.MembershipFee;
 import com.linkdatabase.federationagriculteur.repository.CollectivityRepository;
 import com.linkdatabase.federationagriculteur.repository.MembershipFeeRepository;
+
+import java.util.List;
 
 public class MembershipFeeService {
     private final MembershipFeeRepository membershipFeeRepository;
@@ -11,5 +14,12 @@ public class MembershipFeeService {
                                  CollectivityRepository collectivityRepository) {
         this.membershipFeeRepository = membershipFeeRepository;
         this.collectivityRepository = collectivityRepository;
+    }
+
+    public List<MembershipFee> getMembershipFees(String collectivityId) {
+         collectivityRepository.findById(collectivityId)
+                .orElseThrow(() -> new RuntimeException("Collectivity not found with id: " + collectivityId));
+
+         return membershipFeeRepository.findByCollectivityId(collectivityId);
     }
 }
