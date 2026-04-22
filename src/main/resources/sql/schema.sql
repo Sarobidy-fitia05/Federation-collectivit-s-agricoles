61273e02b29c02f1d63e89eab8e801e5ae438914
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-22 22:24:17 +0300

    add the sql request of financial_account and member_payment

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -8,3 +8,24 @@ CREATE TABLE membership_fee (
                                 label VARCHAR(255) NOT NULL,
                                 status VARCHAR(10) NOT NULL DEFAULT 'ACTIVE'
 );
+-- financial_account
+CREATE TABLE IF NOT EXISTS financial_account (
+                                                 id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    account_number VARCHAR(100),
+    bank_name VARCHAR(255),
+    collectivity_id VARCHAR(255)
+    );
+
+--member_payment
+CREATE TABLE IF NOT EXISTS member_payment (
+                                              id VARCHAR(255) PRIMARY KEY,
+    amount DECIMAL(19,2) NOT NULL,
+    membership_fee_id VARCHAR(255),
+    financial_account_id VARCHAR(255),
+    payment_mode VARCHAR(50),
+    creation_date TIMESTAMP NOT NULL,
+    member_id VARCHAR(255),
+    FOREIGN KEY (membership_fee_id) REFERENCES membership_fee(id),
+    FOREIGN KEY (financial_account_id) REFERENCES financial_account(id)
+    );
