757591b7f43e57028be671730233f25684f13c16
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-22 21:04:48 +0300

    add all attributs inside MemberPayment class

--- src/main/java/com/linkdatabase/federationagriculteur/entity/MemberPayment.java
+++ src/main/java/com/linkdatabase/federationagriculteur/entity/MemberPayment.java
@@ -1,4 +1,18 @@
 package com.linkdatabase.federationagriculteur.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
 public class MemberPayment {
+    private String id;
+    private BigDecimal amount;
+    private String membershipFeeId;
+    private String financialAccountId;
+    private PaymentMode paymentMode;
+    private LocalDateTime creationDate = LocalDateTime.now();
+    private String memberId;
 }
