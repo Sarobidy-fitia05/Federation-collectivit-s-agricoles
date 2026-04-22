abfc83e55732db2fa5ab85fad8e165165ac6fdbe
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-22 23:01:49 +0300

    add id in their attributes

--- src/main/java/com/linkdatabase/federationagriculteur/dto/CreateCollectivityRequest.java
+++ src/main/java/com/linkdatabase/federationagriculteur/dto/CreateCollectivityRequest.java
@@ -1,10 +1,15 @@
 package com.linkdatabase.federationagriculteur.dto;
 
+import com.linkdatabase.federationagriculteur.entity.MemberInformation;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
-public class CreateCollectivityRequest {
+@NoArgsConstructor
+public class CreateCollectivityRequest  {
+    private String id;
     private String location;
     private List<String> members;
     private Boolean federationApproval;
