d967bac2d5c8dd2ba8f9ede8567306fcffdd0241
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-22 15:43:25 +0300

    add constructor of Membershiprepository and CollectivityRepository

--- src/main/java/com/linkdatabase/federationagriculteur/service/MembershipFeeService.java
+++ src/main/java/com/linkdatabase/federationagriculteur/service/MembershipFeeService.java
@@ -1,4 +1,15 @@
 package com.linkdatabase.federationagriculteur.service;
 
+import com.linkdatabase.federationagriculteur.repository.CollectivityRepository;
+import com.linkdatabase.federationagriculteur.repository.MembershipFeeRepository;
+
 public class MembershipFeeService {
+    private final MembershipFeeRepository membershipFeeRepository;
+    private final CollectivityRepository collectivityRepository;
+
+     public MembershipFeeService(MembershipFeeRepository membershipFeeRepository,
+                                 CollectivityRepository collectivityRepository) {
+        this.membershipFeeRepository = membershipFeeRepository;
+        this.collectivityRepository = collectivityRepository;
+    }
 }
