f76295d36c464aa7865f951ae284532f8edaad41
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-23 08:50:55 +0300

    the pathway go to collectivityTransaction is okey

--- src/main/java/com/linkdatabase/federationagriculteur/controller/MemberController.java
+++ src/main/java/com/linkdatabase/federationagriculteur/controller/MemberController.java
@@ -1,13 +1,11 @@
 package com.linkdatabase.federationagriculteur.controller;
 
 import com.linkdatabase.federationagriculteur.dto.CreateMemberRequest;
-import com.linkdatabase.federationagriculteur.entity.CollectivityTransaction;
-import com.linkdatabase.federationagriculteur.entity.Member;
+ import com.linkdatabase.federationagriculteur.entity.Member;
 import com.linkdatabase.federationagriculteur.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -27,18 +25,5 @@ public class MemberController {
         return new ResponseEntity<>(members, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}/transactions")
-    public ResponseEntity<List<CollectivityTransaction>> getCollectivityTransactions(
-            @PathVariable String id,
-            @RequestParam LocalDate from,
-            @RequestParam LocalDate to) {
 
-         if (!collectivityService.existsById(id)) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Collectivity not found");
-        }
-
-         List<CollectivityTransaction> transactions = transactionService.getTransactions(id, from, to);
-
-         return ResponseEntity.ok(transactions);
-    }
 }
